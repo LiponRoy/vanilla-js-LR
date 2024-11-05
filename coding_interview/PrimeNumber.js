9f8b27a7d44d25091cea330535ca850fa89b96ca
lipon <liponbdbd@gmail.com>
2024-11-05 18:41:06 +0600

    added factorial and so on

---
+++ coding_interview/PrimeNumber.js
@@ -0,0 +1,9 @@
+// const PrimeNumber = (num) => {
+//   if (num % 2 === 0) {
+//     console.log("Good for ATM");
+//   } else {
+//     console.log("Not accepted for ATM");
+//   }
+// };
+
+// PrimeNumber(1600);
